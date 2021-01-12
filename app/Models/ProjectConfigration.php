c7816183533b7dc85951b816b0e6f14c7449a621
hossam <hossamameen948@gmail.com>
2021-01-12 16:59:21 +0200

    edit from hossam

--- app/Models/ProjectConfigration.php
+++ app/Models/ProjectConfigration.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectConfigration extends Model
 {
     public  static function get_pull_ratio(){
+        
         return ProjectConfigration::find(1)->pull_ratio;
     }
 }
