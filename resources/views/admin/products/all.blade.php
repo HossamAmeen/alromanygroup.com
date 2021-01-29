43dd9c4cbbb19ab9054c93613e9bd994534f1781
Abdelrahman Asem <abdelrahman.elzedy@gmail.com>
2021-01-30 01:25:37 +0200

    update products all table

--- resources/views/admin/products/all.blade.php
+++ resources/views/admin/products/all.blade.php
@@ -61,7 +61,7 @@
                                                 <td><img src="{{URL::asset($row->qr_code)}}" class="img-responsive" style="width: 100px;" /></td>
                                                 <td>{{\App\Helpers\DateHelper::print_date($row->created_at)}}</td>
                                                 <td class="text-center">
-                                                    <a  href="{{URL::to('admin/product/'.$row->qr_code)}}"><button class="btn btn-xs btn-success" title="تنزيل الباركود"><i class="fa fa-download"></i></button></a>
+                                                    <a  href="{{URL::to('product/'.$row->qr_code)}}"><button class="btn btn-xs btn-success" title="تنزيل الباركود"><i class="fa fa-download"></i></button></a>
                                                     <a  href="{{URL::to('admin/product/'.$row->id .'/update')}}"><button class="btn btn-xs btn-warning" title="تعديل"><i class="fa fa-pencil-square-o"></i></button></a>
                                                     <a class="check" href="{{URL::to('admin/product/'.$row->id .'/delete')}}"><button class="btn btn-xs btn-danger" title="حذف"><i class="fa fa-minus"></i></button></a>
                                                 </td>
